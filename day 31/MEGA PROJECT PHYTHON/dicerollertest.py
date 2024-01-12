ff84751e6c9aa22da684b1c1ebb4c114855733e1
Vanosultanishvili <148572577+Vanosultanishvili@users.noreply.github.com>
2024-01-12 23:36:40 +0400

    Add files via upload

---
+++ day 31/MEGA PROJECT PHYTHON/dicerollertest.py
@@ -0,0 +1,59 @@
+name = input("enter your name:")
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+def rolldice():
+  import random
+  dice1 = random.randint(1,6)
+  dice2 = random.randint(1,6)
+  print("computer rolled", dice1)
+  print(name + " rolled", dice2)
+
+rolldice()
+
+
+
+
+
