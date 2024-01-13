465b1b409361ad02bb9e6c0ccaca1d01995219bf
Vanosultanishvili <148572577+Vanosultanishvili@users.noreply.github.com>
2024-01-13 10:06:30 +0400

    Update dicerollertest.py

--- day 31/MEGA PROJECT PHYTHON/dicerollertest.py
+++ day 31/MEGA PROJECT PHYTHON/dicerollertest.py
@@ -1,57 +1,26 @@
-name = input("enter your name:")
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
-
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
-
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
-
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
-
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
-
-def rolldice():
-  import random
-  dice1 = random.randint(1,6)
-  dice2 = random.randint(1,6)
-  print("computer rolled", dice1)
-  print(name + " rolled", dice2)
-
-rolldice()
+name =input ("Enter your name:")
+user_score =0 
+computer_score =0 
+def rolldice ():
+    global user_score 
+    global computer_score 
+    import random 
+    user =random .randint (1 ,6 )
+    computer =random .randint (1 ,6 )
+    print ("Computer rolled",user )
+    print (name +" rolled",computer )
+    if user >computer :
+        computer_score +=1 
+        print ("Computer wins this round!")
+    elif user <computer :
+        user_score +=1 
+        print (name +" wins this round!")
+    else :
+        print ("It's a tie!")#line:20
+    print (f"Score - {name}: {user_score}, Computer: {computer_score}")
+for _ in range (6 ):
+    rolldice ()
+ 
 
 
 
